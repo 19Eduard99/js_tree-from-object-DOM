6eaa395d9da4b24393ce86b7b53721fb9e843c0e
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-09-13 11:27:29 +0300

    make build

---
+++ index.111f32b2.js
@@ -0,0 +1,2 @@
+!function e(n,t){for(var r in t){var a=document.createElement("li");if(a.textContent=r,"object"==typeof t[r]&&null!==t[r]&&Object.keys(t[r]).length>0){var l=document.createElement("ul");e(l,t[r]),a.append(l)}n.append(a)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.111f32b2.js.map
